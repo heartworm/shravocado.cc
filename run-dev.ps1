5068b1d8c15f010346eb96b5dd45786d3e17f689
Shravan Lal <shravanlal98@gmail.com>
2018-02-12 21:18:49 +1000

    cv

--- run-dev.ps1
+++ run-dev.ps1
@@ -3,4 +3,3 @@ docker stop $all_containers
 docker rm $all_containers
 docker-compose build
 docker run -v "$(gl)\nginx\nginx-root:/usr/share/nginx/html" --add-host "backend:172.22.111.1" -dp 80:80 shravocadocc_web
-python ./backend/main.py
